69e6972d9f49591b159b44dd84fb7a6421f361b2
Jan Marko Mühlnikel <jan.muehlnikel@gmx.de>
2024-07-03 21:39:52 +0200

    changed reward function

--- freeway/dqn/modules/rewards.py
+++ freeway/dqn/modules/rewards.py
@@ -1,43 +1,57 @@
-def action_based_reward(total_reward, crashed, action, y_pos, max_distance_episode):
+def action_based_reward(total_reward, crashed, action, y_pos, game_reward):
+    total_reward_beginning = total_reward
     # REWARDS
-    crash = -30
-    finishing = 50
-    half = 25
-    quater = 10
+    crash = -500
+    finishing = 1000
+    half = 10
+    quater = 5
 
-    up = 1
-    down = -1
-    nothing = -0.5
+    up = 20
+    down = -20
+    nothing = -10
 
     #apply rewards
     if action == 0:
         total_reward += nothing
+        #print(f"nothing: {nothing} ,{y_pos}")
     elif action == 1:
         total_reward += up
+        #print(f"up: {up} ,{y_pos}")
     elif action == 2:
         total_reward += down
-
+        #print(f"down: {down} ,{y_pos}")
+    """
     # rewwad for gaining forward progress
     if y_pos > max_distance_episode:
         total_reward += max_distance_episode
-
+        print(f"max distance {max_distance_episode}")
+    """
     # negative reward for death
     if crashed == 1:
         total_reward += crash
-
+       # print(f"crash {crash} ,{y_pos}")
+    """
     # reward chicken reaching quater
-    if y_pos == 42:
+    if y_pos == 16 and reached_quater == False:
         total_reward += quater
-
+        print(f"rached quater: {quater} ,{y_pos}")
+    """
+    # reward chicken for being over quater
+    if y_pos >= 16:
+        total_reward += quater
+        #print(f"rached quater: {quater} ,{y_pos}")
     # reward chicken reaching half
-    if y_pos == 86:
+    if y_pos >= 32:
         total_reward += half
-
+        #print(f"rached half: {half} ,{y_pos}")
     # reward chicken reaching end
-    if y_pos >= 175:
+    if game_reward == 1:
         total_reward += finishing
+        #print(f"finsihed: {finishing} ,{y_pos}")
 
-    return total_reward
+    gained_reward = total_reward - total_reward_beginning
+
+    return total_reward, gained_reward
     
 
 
